fbc66175d8c725c82c99fbedc1ecc79ecbc04d69
Davron <davron1104@yandex.com>
2022-11-01 22:49:40 +0500

    second commit

--- js/script.js
+++ js/script.js
@@ -1,2 +1,24 @@
 'use strict';
 
+const box = document.getElementById('box'),
+      btns = document.getElementsByTagName('button'),
+      circles = document.getElementsByClassName('circle'),
+      hearts = document.querySelectorAll('.heart'),
+      oneHeart = document.querySelector('.heart');
+
+// box.style.backgroundColor = 'blue';
+// box.style.width = '100%';
+box.style.cssText = 'background-color: blue; width: 100%;';
+btns[2].style.borderRadius = '250px';
+btns[2].style.backgroundColor = 'red';
+circles[0].style.backgroundColor = 'yellow';
+
+// for(let i = 0; i < hearts.length; i++) {
+//     hearts[i].style.backgroundColor = 'blue';
+// }
+
+hearts.forEach(item => {
+    item.style.backgroundColor = 'black';
+});
+
+
